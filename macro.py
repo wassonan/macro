08f2e541bf94ef797dd07171e29b338a61d67c4f
Wasson An <wassonan257@gmail.com>
2016-01-28 09:41:14 -0500

    Created learning file and started macro

--- macro.py
+++ macro.py
@@ -1,1 +1,23 @@
+from evdev import InputDevice, list_devices, categorize, ecodes, UInput, ecodes\
+    as e, InputEvent
+import time
 
+devices = [InputDevice(fn) for fn in list_devices()]
+
+for i in range(len(devices)):
+    print i, ": " , devices[i].name
+
+choice = int(input())
+chosenDevice = devices[choice]
+
+print "You chose" , devices[choice].name
+#TODO Need to figure out a better way to prevent previous key presses
+time.sleep(1)
+print "Please press program macro key"
+chosenDevice = InputDevice(list_devices()[choice])
+
+#TODO read() seems to throw an IOError using read_loop() and break temporarily
+for event in chosenDevice.read_loop():
+    if event.type == e.EV_KEY:
+        print(categorize(event))
+        break;
---
+++ macrolearn.py
@@ -0,0 +1,46 @@
+from evdev import InputDevice, list_devices, categorize, ecodes, UInput, ecodes\
+    as e
+
+devices = [InputDevice(fn) for fn in list_devices()]
+
+for dev in devices:
+    print(dev.fn, dev.name, dev.phys)
+
+dev = InputDevice("/dev/input/event2")
+print dev
+
+ui = UInput()
+ui.write(e.EV_KEY, e.KEY_H, 1)
+ui.write(e.EV_KEY, e.KEY_H, 0)
+ui.write(e.EV_KEY, e.KEY_E, 1)
+ui.write(e.EV_KEY, e.KEY_E, 0)
+ui.write(e.EV_KEY, e.KEY_L, 1)
+ui.write(e.EV_KEY, e.KEY_L, 0)
+ui.write(e.EV_KEY, e.KEY_L, 1)
+ui.write(e.EV_KEY, e.KEY_L, 0)
+ui.write(e.EV_KEY, e.KEY_O, 1)
+ui.write(e.EV_KEY, e.KEY_O, 0)
+ui.write(e.EV_KEY, e.KEY_SPACE, 1)
+ui.write(e.EV_KEY, e.KEY_SPACE, 0)
+ui.write(e.EV_KEY, e.KEY_W, 1)
+ui.write(e.EV_KEY, e.KEY_W, 0)
+ui.write(e.EV_KEY, e.KEY_O, 1)
+ui.write(e.EV_KEY, e.KEY_O, 0)
+ui.write(e.EV_KEY, e.KEY_R, 1)
+ui.write(e.EV_KEY, e.KEY_R, 0)
+ui.write(e.EV_KEY, e.KEY_L, 1)
+ui.write(e.EV_KEY, e.KEY_L, 0)
+ui.write(e.EV_KEY, e.KEY_D, 1)
+ui.write(e.EV_KEY, e.KEY_D, 0)
+ui.write(e.EV_KEY, e.KEY_RIGHTSHIFT, 1)
+ui.write(e.EV_KEY, e.KEY_1, 1)
+ui.write(e.EV_KEY, e.KEY_RIGHTSHIFT, 0)
+ui.write(e.EV_KEY, e.KEY_1, 0)
+ui.write(e.EV_KEY, e.KEY_ENTER, 1)
+ui.write(e.EV_KEY, e.KEY_ENTER, 0)
+ui.syn()
+ui.close()
+
+for event in dev.read_loop():
+    if event.type == ecodes.EV_KEY:
+        print(categorize(event))
