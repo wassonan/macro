78147a8ee261ffeb0dbd032e31206a830b5176f7
Wasson An <wassonan257@gmail.com>
2016-02-01 11:03:31 -0500

    Added save and load functionality to the macro class

--- macro.py
+++ macro.py
@@ -6,9 +6,6 @@ class Macro:
         self.macroKey = mK
         self.keySequence = kS
         self.keyState = kSS
-        print
-        print len(self.keySequence)
-        print len(self.keyState)
 
     def __str__(self):
         toReturn = "Macro Key: " + str(e.KEY[self.macroKey])
@@ -34,7 +31,34 @@ class Macro:
         ui.syn()
         ui.close()
 
+    def saveProfile(self, profileName):
+        f = open(profileName + ".mp", "w")
+        f.write(str(self.macroKey) + "\n")
+        for i in range(len(self.keySequence)):
+            f.write(str(self.keySequence[i]) + ":" + str(self.keyState[i]) + "\n");
+        f.close()
+
+    @staticmethod
+    def loadProfile(profileName):
+        f = open(profileName + ".mp", "r")
+        string = f.readline().rstrip("\n")
+        macroKey = int(string)
+        keySequence = []
+        keyState = []
+        string = f.readline().rstrip("\n")
+        while string != "":
+            index = string.index(":")
+            keySequence.append(int(string[:index]))
+            keyState.append(int(string[index + 1:]))
+            string = f.readline().rstrip("\n")
+        f.close()
+        return Macro(macroKey, keySequence, keyState)
+
 if __name__ == "__main__":
     testMacro = Macro(2, [3, 3, 4, 4, 5, 5, 6, 6], [1, 0, 1, 0, 1, 0, 1, 0])
     print testMacro
     testMacro.execute()
+    testMacro.saveProfile("test")
+    saveMacro = Macro.loadProfile("test")
+    print saveMacro
+    saveMacro.execute()
