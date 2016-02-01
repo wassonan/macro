1c0fd94e162ec9838f2fb34f22572ee0de0c4752
Wasson An <wassonan257@gmail.com>
2016-02-01 12:27:09 -0500

    Restructured macrodriver.py to prepare for menu support. Not Working

--- macrodriver.py
+++ macrodriver.py
@@ -1,77 +1,87 @@
 #!/usr/bin/python
+import sys
 from macro import Macro
 from evdev import InputDevice, list_devices, categorize, UInput, ecodes as e,\
     InputEvent, events
 
-devices = [InputDevice(fn) for fn in list_devices()]
+# TODO Need to make this as a seperate function. Need to add a way to exit this
+# function to return to the main menu. Currently planned way to exit to menu is 
+# to press macro key 3 times in a row
+def readInputs(chosenDevice, macros)
+    recording = False
+    currentMacroKey = None;
+    currentMacro = []
+    currentState = []
+    numKey = 0
 
-for i in range(len(devices)):
-    print i, ": " , devices[i].name
+    for event in chosenDevice.read_loop():
+        if event.type == e.EV_KEY:
 
-choice = int(input())
-chosenDevice = devices[choice]
+            # If the program macro key is hit
+            if event.code == programMacroKey:
+                if event.value == events.KeyEvent.key_down:
+                    if not recording:
+                        print "Recording"
+                        recording = True
 
-print "You chose" + devices[choice].name
-for device in devices:
-    device.close()
-chosenDevice = InputDevice(list_devices()[choice])
+                    elif currentMacroKey == None or currentMacro == []:
+                            print "Recording canceled"
+                            recording = False
+                            currentMacroKey = None
+                            currentMacro = []
+                            currentMacroState = []
 
-print "Please press program macro key"
-programMacroKey = None
+                    else:
+                        newMacro = Macro(currentMacroKey, currentMacro, currentState)
+                        print "Recording saved: " + str(newMacro)
 
-while programMacroKey == None:
-    event = chosenDevice.read_one()
-    if event != None:
-        if event.type == e.EV_KEY and event.value == events.KeyEvent.key_down:
-            programMacroKey = event.code
-
-print e.KEY[programMacroKey]
-
-recording = False
-currentMacroKey = None;
-currentMacro = []
-currentState = []
-macros = []
-
-
-for event in chosenDevice.read_loop():
-    if event.type == e.EV_KEY:
-
-        # If the program macro key is hit
-        if event.code == programMacroKey:
-            if event.value == events.KeyEvent.key_down:
-                if not recording:
-                    print "Recording"
-                    recording = True
-
-                elif currentMacroKey == None or currentMacro == []:
-                        print "Recording canceled"
+                        macros.append(newMacro)
                         recording = False
                         currentMacroKey = None
                         currentMacro = []
-                        currentMacroState = []
+                        currentState = []
 
-                else:
-                    newMacro = Macro(currentMacroKey, currentMacro, currentState)
-                    print "Recording saved: " + str(newMacro)
-
-                    macros.append(newMacro)
-                    recording = False
-                    currentMacroKey = None
-                    currentMacro = []
-                    currentState = []
-
-        # If any other key is hit
-        else:
-            if recording:
-                if currentMacroKey == None:
-                    currentMacroKey = event.code
-
-                else:
-                    currentMacro.append(event.code)
-                    currentState.append(event.value)
-
+            # If any other key is hit
             else:
-                for macro in macros:
-                    if macro.macroKey == event.code:
-                        macro.execute()
+                if recording:
+                    if currentMacroKey == None:
+                        currentMacroKey = event.code
+
+                    else:
+                        currentMacro.append(event.code)
+                        currentState.append(event.value)
+
+                else:
+                    for macro in macros:
+                        if macro.macroKey == event.code:
+                            macro.execute()
+
+if __name__ == "__main__":
+    # TODO Should add in fucntion parameters. These should include things such as
+    # auto load profile, and quick start-up
+
+    devices = [InputDevice(fn) for fn in list_devices()]
+
+    for i in range(len(devices)):
+        print i, ": " , devices[i].name
+
+    choice = int(input())
+    chosenDevice = devices[choice]
+
+    print "You chose " + devices[choice].name
+    for device in devices:
+        device.close()
+    chosenDevice = InputDevice(list_devices()[choice])
+
+    print "Please press program macro key"
+    programMacroKey = None
+
+    while programMacroKey == None:
+        event = chosenDevice.read_one()
+        if event != None:
+            if event.type == e.EV_KEY and event.value == events.KeyEvent.key_down:
+                programMacroKey = event.code
+
+    print e.KEY[programMacroKey]
+
+# TODO Need to create the main menu and call the readInputs function
